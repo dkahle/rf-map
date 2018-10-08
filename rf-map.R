4ff51a991655ed538efa16490857458b3b110a5c
David Kahle <david.kahle@gmail.com>
2018-10-08 08:18:58 -0500

    use correct files from bill

--- rf-map.R
+++ rf-map.R
@@ -1,12 +1,14 @@
-# This script is for applying the RFMAP model 
-# The following code will build the model using the BU-SI uppermost B horizon data set
-# An example is provided that will facilitate applicaiton of the model on new data
+# This script will re-create a version of the RF-MAP model on your system. It is associated with the following manuscript:
+# Lukens, W.E., Stinchcomb, G.E., Nordt, L.C., Driese, S.G., Kahle, D.J., and Tubbs, J.D., 
+# Recursive partitioning improves paleosol estimates for rainfall, 
+# submitted to the American Journal of Science, October 2018
+
+# The following code will build the RF-MAP model using the BU-SI uppermost B horizon data set
 
 #### I. Setup ####
 # Set your working directory: change the file path to the folder that contains your data files (.csv format)
 # Note: if you are using a PC, make sure you change backslashes to forward slashes
 setwd("C:/.../RFMAP")
-setwd("/Users/williamlukens/Desktop/RFMAP")
 
 # Read in your data file
 BUSI <- read.csv("BUSI.csv", header = TRUE, na.strings=c("", " ", "NA")) 
@@ -18,7 +20,6 @@ require(randomForest)
 
 # Generate the RFMAP model
 RFMAP <- randomForest(MAP ~ ., data = BUSI, importance=TRUE) 
-RFMAP #Summary of the model; use a prediction error of 442 mm
 
 #### Apply the RFMAP model on new data ####
 # First load in your new data. The .csv file is named "Input.csv"
@@ -26,12 +27,15 @@ RFMAP #Summary of the model; use a prediction error of 442 mm
 Input <- read.csv("Input.csv", header = TRUE, na.strings=c("", " ", "NA"))
 
 # Predict MAP values
-Output <- predict(RFMAP, newdata=Input)
+Output <- Input
+Output$RFMAP <- predict(RFMAP, newdata=Input) # prediction error = 440 mm
 
-# Save results as an output file; the second column will be your MAP values
+# Save results as an output file; the RFMAP column will contain your MAP values
 write.csv(Output, "Output.csv")
+# The Output.csv file is now located in your directory folder.
+# Please use a prediction error of 440 mm associated with all output values.
 
-# This code is part of the following manuscript:
-# Lukens, W.E., Stinchcomb, G.E., Nordt, L.C., Driese, S.G., Kahle, D.J., and Tubbs, J.D., 
-# Recursive partitioning improves paleosol estimates for rainfall, 
-# submitted to the American Journal of Science, October 2018
+# Please note: each version of the RF-MAP model that is re-created will be slightly different from all other versions.
+# This means that the exact predicted values will likely differ from other versions, but will be quite close and
+# within error of one another. Please consult the authors with any questions or bugs.
+
