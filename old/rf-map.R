7de122e74cccf25c084b995ff66e68a2248a93c5
David Kahle <david.kahle@gmail.com>
2019-09-18 09:16:40 -0500

    fix files

---
+++ old/rf-map.R
@@ -0,0 +1,58 @@
+# This script will re-create a version of the RF-MAP model on your system. It is associated with the following manuscript:
+# Lukens, W.E., Stinchcomb, G.E., Nordt, L.C., Driese, S.G., Kahle, D.J., and Tubbs, J.D., 
+# Recursive partitioning improves paleosol estimates for rainfall, 
+# submitted to the American Journal of Science, October 2018
+
+# The following code will build the RF-MAP model using the BU-SI uppermost B horizon data set
+
+
+
+#### I. Setup ####
+# Set your working directory: change the file path to the folder that contains your data files (.csv format)
+# Note: if you are using a PC, make sure you change backslashes to forward slashes
+if (!requireNamespace("here")) install.packages("here")
+library("here")
+
+
+
+# Read in your data file
+busi <- read.csv(
+  file = here("busi.csv"), 
+  header = TRUE, na.strings = c("", " ", "NA")
+) 
+
+
+
+#### Build the RFMAP model ####
+
+# Install the randomForest package, if needed
+if (!requireNamespace("randomForest")) install.packages("randomForest")
+library("randomForest")
+
+# Generate the RFMAP model
+set.seed(42L)
+(rfmap <- randomForest(MAP ~ ., data = busi, importance = TRUE))
+
+
+
+#### Apply the RFMAP model on new data ####
+# First load in your new data. The .csv file is named "input.csv"
+# You can load in any csv file, but make sure you update the file name in the next line:
+input <- read.csv(
+  file = here("input.csv"), 
+  header = TRUE, na.strings = c("", " ", "NA")
+)
+
+# Predict MAP values
+output <- input
+output$rfmap <- predict(rfmap, newdata = input) # prediction error = 440 mm
+
+# Save results as an output file; the RFMAP column will contain your MAP values
+write.csv(output, here("output.csv"))
+# The output.csv file is now located in your directory folder.
+# Please use a prediction error of 440 mm associated with all output values.
+
+# Please note: each version of the RF-MAP model that is re-created will be slightly different from all other versions.
+# This means that the exact predicted values will likely differ from other versions, but will be quite close and
+# within error of one another. Please consult the authors with any questions or bugs.
+
--- rf-map.R
+++ rf-map.R
@@ -6,21 +6,21 @@
 # The following code will build the RF-MAP model using the BU-SI uppermost B horizon data set
 
 
-
 #### I. Setup ####
-# Set your working directory: change the file path to the folder that contains your data files (.csv format)
-# Note: if you are using a PC, make sure you change backslashes to forward slashes
+# The 'here' package should take care of file pathing.
 if (!requireNamespace("here")) install.packages("here")
 library("here")
 
-
-
-# Read in your data file
+# Read in your data files
 busi <- read.csv(
   file = here("busi.csv"), 
   header = TRUE, na.strings = c("", " ", "NA")
 ) 
 
+busi1600 <- read.csv(
+  file = here("busi1600.csv"), 
+  header = TRUE, na.strings = c("", " ", "NA")
+) 
 
 
 #### Build the RFMAP model ####
@@ -31,8 +31,8 @@ library("randomForest")
 
 # Generate the RFMAP model
 set.seed(42L)
-(rfmap <- randomForest(MAP ~ ., data = busi, importance = TRUE))
-
+(rfmap1.0 <- randomForest(MAP ~ ., data = busi, importance = TRUE))
+(rfmap2.0 <- randomForest(MAP ~ ., data = busi1600, importance = TRUE))
 
 
 #### Apply the RFMAP model on new data ####
@@ -45,14 +45,15 @@ input <- read.csv(
 
 # Predict MAP values
 output <- input
-output$rfmap <- predict(rfmap, newdata = input) # prediction error = 440 mm
+output$rfmap <- ifelse(output$version == "2", predict(rfmap2.0, newdata = input),
+                       predict(rfmap1.0, newdata = input))
+output$rfmap <- round(output$rfmap, 0)
+output$error <- ifelse(output$version == "2", "209", "395")
+
 
 # Save results as an output file; the RFMAP column will contain your MAP values
 write.csv(output, here("output.csv"))
+
 # The output.csv file is now located in your directory folder.
-# Please use a prediction error of 440 mm associated with all output values.
-
-# Please note: each version of the RF-MAP model that is re-created will be slightly different from all other versions.
-# This means that the exact predicted values will likely differ from other versions, but will be quite close and
-# within error of one another. Please consult the authors with any questions or bugs.
-
+# Please use a prediction error of 395 mm associated with all output values for RF-MAP1.0.
+# Please use a prediction error of 209 mm associated with all output values for RF-MAP2.0.
